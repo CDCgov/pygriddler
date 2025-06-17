1e4a283de4a57d78b2b4785d22f95673d132ea9c
Scott Olesen <ulp7@cdc.gov>
2025-06-17 17:53:55 -0400

    Add all tests

--- tests/griddle/test_v03.py
+++ tests/griddle/test_v03.py
@@ -17,3 +17,46 @@ def test_simple():
     parameters:
       R0: {fix: 1.0}
     """) == [{"R0": 1.0}]
+
+
+def test_vary():
+    assert text_to_dicts("""
+    schema: v0.3
+    parameters:
+      R0: {vary: [1.5, 2.0]}
+      gamma: {vary: [0.3, 0.4]}
+    """) == [
+        {"R0": 1.5, "gamma": 0.3},
+        {"R0": 1.5, "gamma": 0.4},
+        {"R0": 2.0, "gamma": 0.3},
+        {"R0": 2.0, "gamma": 0.4},
+    ]
+
+
+def test_bundle():
+    assert text_to_dicts("""
+    schema: v0.3
+    parameters:
+      scenario:
+        R0: [low, high]
+        gamma: [low, high]
+    """) == [{"R0": "low", "gamma": "low"}, {"R0": "high", "gamma": "high"}]
+
+
+def test_conditional():
+    assert text_to_dicts("""
+    schema: v0.3
+    parameters:
+    method: {vary: [newton, brent]}
+    start_point:
+        if: {equals: {method: newton}}
+        vary: [0.25, 0.50, 0.75]
+    bounds:
+        if: {equals: {method: brent}}
+        fix: [0.0, 1.0]
+    """) == [
+        {"method": "newton", "start_point": 0.25},
+        {"method": "newton", "start_point": 0.50},
+        {"method": "newton", "start_point": 0.75},
+        {"method": "brent", "bounds": [0.0, 1.0]},
+    ]
